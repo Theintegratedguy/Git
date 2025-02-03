743bb25eae0af797e0f9c6596a61bcd55d018655
Rohan <rohan05bhola@gmail.com>
2025-02-03 16:33:59 +0530

    changed the Main.java file

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
+import java.util.HashSet;
+import java.util.Set; 
+
 public class Main {
     public static void main(String[] args){
-        int[] arr = new int[10]; 
-        for(int i=0; i<arr.length;  i++){
-            System.out.print(arr[i]+" ");
+        Set<Integer> set = new HashSet<>(); 
+        set.add(1); 
+        set.add(2); 
+        set.add(3); 
+        set.add(4); 
+
+        if(set.contains(3)){
+            System.out.println("yes, 3 is present"); 
+        }
+        else{
+            System.out.println("no, the number-3 is not present"); 
         }
     }
 }
